8cd7785953d1a797009e45d104529a3c70c04961
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-18 15:28:21 +0900

    content-Type 작성

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
-const 
+const contentType = {
+  'content-Type' : 'text.html',
+  'charset' : 'utf-8'
+}
+
+const server = http.createServer((request, response) => {
+  if(request.method === 'GET' && request.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if(err) {
+        console.log('파일 호출 에러');
+      } else {
+        response.writeHead(200, contentType);
+        response.end(data);
+      }
+    })
+  }
+});
+  server.listen(8080);
