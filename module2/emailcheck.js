248b4429b256b1a1ab0326148ecfff42e2cf82dc
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-20 15:26:33 +0900

    email 조건문 작성완료

--- module2/emailcheck.js
+++ module2/emailcheck.js
@@ -1,13 +1,15 @@
-// mail은 매개변수, 
+// mail은 매개변수 
 function emailCheck(mail) {
+  // 찾는 문자열이 없으면 -1
+  // false 반환해라.
   if(mail.indexOf("@") === -1 ){
     return false;
   }
   if(mail.indexOf(".") === -1){
     return false;
-  }
-}
-  
+  } 
+  return true;
+} ;
 
 email.indexOf("@");
 email.indexOf(".");
